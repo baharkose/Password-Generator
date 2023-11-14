22a1ce67a5c3c0b0817e0e8d8cf70b4f312103a7
baharkose <baharkse17@gmail.com>
2023-11-14 21:49:36 +0300

    added form range output to before

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const displayResult = document.getElementById("display-result");
 const createBtn = document.querySelector(".create");
+const refreshBtn = document.querySelector(".refresh")
 
 const symbols = "!@#$%^&*()\\_+~|}{[]:;?><,./-=";
 const numbers = "0123456789";
@@ -7,42 +8,55 @@ const alphabets = "abcdefghijklmnopqrstuvwxyz";
 
 
 createBtn.addEventListener("click", () => {
-
-  let password = [];
-  let countNumbers = 0;
-  let countSymbols = 0;
-
-  while ((password.length < 10) && ((countNumbers < 2) || (countSymbols < 3))) {
-        let randomChoice = Math.floor(Math.random() * 3);
-        let choice;
-
-        if (randomChoice === 0 && countSymbols < 2) {
-
-                choice = symbols;
-                countSymbols++;
-
-        } else if (randomChoice === 1 && countNumbers < 3) {
-
-                choice = numbers;
-                countNumbers++;
-
-        } else {
-                choice = alphabets;
-                if (Math.round(Math.random())) {
-                    choice = choice.toUpperCase();
-                }
-        }
-
-        let rastgeleIndex = Math.floor(Math.random() * choice.length);
-                password.push(choice[rastgeleIndex]);
-        }
-
-        console.log(`Şifre ${countNumbers} adet sayı içeriyor.`);
-        console.log(`Şifre ${countSymbols} adet sembol içeriyor.`);
-        console.log(password);
-
-
-
-        displayResult.textContent = password.join('');
-        
+        createPassword();
 });
+
+refreshBtn.addEventListener("click", () => {
+    createPassword();
+})
+
+
+
+
+
+
+function createPassword () {
+            
+        let password = [];
+        let countNumbers = 0;
+        let countSymbols = 0;
+
+        while ((password.length < 10) && ((countNumbers < 2) || (countSymbols < 3))) {
+                let randomChoice = Math.floor(Math.random() * 3);
+                let choice;
+
+                if (randomChoice === 0 && countSymbols < 2) {
+
+                        choice = symbols;
+                        countSymbols++;
+
+                } else if (randomChoice === 1 && countNumbers < 3) {
+
+                        choice = numbers;
+                        countNumbers++;
+
+                } else {
+                        choice = alphabets;
+                        if (Math.round(Math.random())) {
+                            choice = choice.toUpperCase();
+                        }
+                }
+
+                let rastgeleIndex = Math.floor(Math.random() * choice.length);
+                        password.push(choice[rastgeleIndex]);
+                }
+
+                console.log(`Şifre ${countNumbers} adet sayı içeriyor.`);
+                console.log(`Şifre ${countSymbols} adet sembol içeriyor.`);
+                console.log(password);
+
+
+
+                displayResult.textContent = password.join('');
+
+}
